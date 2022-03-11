22f29f4c87d6f751ccc1304f37aff6fcdbf3659f
Kevin Phan <kphan489@gmail.com>
2022-03-11 01:00:57 -0600

    +First commit!

---
+++ Werrdd App/DefinitionCard.swift
@@ -0,0 +1,19 @@
+//
+//  DefinitionCard.swift
+//  Werrdd App
+//
+//  Created by Kevin Phan on 3/10/22.
+//
+
+import UIKit
+
+class DefinitionCard: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Werrdd App/ViewController.swift
+++ Werrdd App/ViewController.swift
@@ -8,12 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        addSubViews()
     }
+    
+    func addSubViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(definitionCard)
+        view.addSubview(word)
+        view.addSubview(wordType)
+        view.addSubview(definition)
+        
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            definitionCard.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            definitionCard.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            definitionCard.heightAnchor.constraint(equalToConstant: 200),
+            definitionCard.widthAnchor.constraint(equalToConstant: 350),
+            word.topAnchor.constraint(equalTo: definitionCard.topAnchor, constant: 10),
+            word.leadingAnchor.constraint(equalTo: definitionCard.leadingAnchor, constant: 10),
+            wordType.leadingAnchor.constraint(equalTo: word.trailingAnchor, constant: 5),
+            wordType.topAnchor.constraint(equalTo: definitionCard.topAnchor, constant: 15),
+            definition.topAnchor.constraint(equalTo: word.bottomAnchor, constant: 10),
+            definition.leadingAnchor.constraint(equalTo: definitionCard.leadingAnchor, constant: 5),
+            definition.trailingAnchor.constraint(equalTo: definitionCard.trailingAnchor)
 
-
+        ])
+    }
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.text = "Werrdd"
+        return label
+    }()
+    
+    lazy var definitionCard : UIView = {
+        let card = UIView()
+        card.translatesAutoresizingMaskIntoConstraints = false
+        card.layer.cornerRadius = 25
+        card.backgroundColor = .systemMint
+        return card
+    }()
+    
+    lazy var word: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "GillSans-Bold", size: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Michael Jordan"
+        return label
+    }()
+    
+    lazy var wordType: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Noun"
+        label.font = UIFont.italicSystemFont(ofSize: 12)
+        return label
+    }()
+    
+    lazy var definition: UILabel = {
+        let definition = UILabel()
+        definition.translatesAutoresizingMaskIntoConstraints = false
+        definition.lineBreakMode = .byWordWrapping
+        definition.numberOfLines = 5
+        definition.text = "🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐🐐"
+        return definition
+    }()
 }
 
